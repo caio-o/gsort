853645845d4bf9c01385b21bc99ea0583ec6ee8b
caio-o <caiusbarbieri@gmail.com>
2025-10-08 15:10:25 -0400

    Write bubble sort test

--- sort-t.c
+++ sort-t.c
@@ -20,7 +20,7 @@
 #include <errno.h>
 #include <unistd.h>
 
-#include <sort-t.h>
+#include "sort-t.h"
 
 /**
  * bubble_sort function implementation.
@@ -32,16 +32,16 @@ void bubble_sort (void* base,      // Vector to be sorted
 		  size_t size,     // Size of vector elements
 		  int (*compare) (const void*, const void*)) // Ordering relation
 {
-	size_t i, j, aux;
+	size_t i, j /*, aux*/;
 	char* cBase = (char*) base;	
 
-	for (i = 0; i < n_items-1; i++)
+	for (i = 0; i < n_items; i++)
 	{
-		for (j = 0; j < n_items - i - 1; j++)
+		for (j = i; j > 0; j--)
 		{
-			if (compare (&(cBase[j*size]), &(cBase[(j+1)*size])) > 0)
+			if (compare (&(cBase[j*size]), &(cBase[(j-1)*size])) < 0)
 			{
-				swap (&(cBase[j*size]), &(cBase[(j+1)*size]), size);	
+				swap (&(cBase[j*size]), &(cBase[(j-1)*size]), size);	
 			}
 		}
 	}
--- test.c
+++ test.c
@@ -1,12 +1,44 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <errno.h>
 #include "test.h"
 #include "compare.h"
 #include "sort-t.h"
 
+#define TEST_SIZE 100
+
 int test (void) {
-	printf ("TEST!\n");	
+	int i;
+	int *v = (int*) malloc (sizeof (int) * TEST_SIZE);
+
+	srand (time (NULL));
+
+	if (v) {
+		printf("\n\n");
+
+		for (i = 0; i < TEST_SIZE; i++) {
+			v[i] = random() % 1000;
+			printf ("%d ", v[i]);
+		}
+
+		printf ("\n\n");
+		fgetc (stdin);
+		
+		bubble_sort (v, TEST_SIZE, sizeof (int), intcmp);
+
+		fgetc (stdin);
+
+		for (i = 0; i < TEST_SIZE; i++) {
+			printf ("%d ", v[i]);
+		}
+
+	}
+	else {
+		perror ("test");
+		exit (1);
+	}
 
 	return 0;
 }
